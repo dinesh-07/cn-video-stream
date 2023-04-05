8e84361f9bbed4f26817d0da928d3bbcfa197333
Dinesh Kini Bailoor <dinesh.skini@gmail.com>
2023-04-04 22:31:31 -0400

    save video on sql

--- client/script.js
+++ client/script.js
@@ -1,83 +1,127 @@
 const constraints = {
-    video: {
-      width: {exact: 1280},
-      height: {exact: 720},
-      frameRate: {exact: 30},
-    }
+  video: {
+    width: { exact: 1280 },
+    height: { exact: 720 },
+    frameRate: { exact: 30 },
+  },
+};
+
+let recordedChunks = [];
+let mediaRecorder;
+let theStream;
+
+let stopInterval;
+let count = 0;
+
+function startFunction() {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(gotMedia)
+    .catch((e) => {
+      console.error("getUserMedia() failed: " + e);
+    });
+}
+
+function gotMedia(stream) {
+  theStream = stream;
+  var video = document.querySelector("video");
+  video.srcObject = stream;
+  try {
+    mediaRecorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+  } catch (e) {
+    console.error("Exception while creating MediaRecorder: " + e);
+    return;
+  }
+  theRecorder = mediaRecorder;
+  mediaRecorder.ondataavailable = (event) => {
+    recordedChunks.push(event.data);
   };
-  
-  let recordedChunks = [];
-  let mediaRecorder;
-  let theStream;
+  mediaRecorder.start(100);
 
-  let stopInterval;
-  let count = 0;
+  stopInterval = setInterval(() => {
+    count++;
+    const formData = new FormData();
+    const blob = new Blob(recordedChunks, { type: "video/webm" });
+    formData.append("video", blob, "myvideo.webm");
+    fetch("http://localhost:5000/segment", {
+      method: "POST",
+      headers: {
+        "X-segment-id": count,
+      },
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((response) => console.log("Success:", JSON.stringify(response)))
+      .catch((error) => console.error("Error:", error));
 
-  function startFunction() {
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(gotMedia)
-        .catch(e => { console.error('getUserMedia() failed: ' + e); });
-  }
-
-  function gotMedia(stream) {
-    theStream = stream;
-    var video = document.querySelector('video');
-    video.srcObject = stream;
-    try {
-        mediaRecorder = new MediaRecorder(stream, {mimeType : "video/webm"});
-    } catch (e) {
-      console.error('Exception while creating MediaRecorder: ' + e);
-      return;
-    }
-    theRecorder = mediaRecorder;
-    mediaRecorder.ondataavailable = 
-        (event) => { recordedChunks.push(event.data); };
-        mediaRecorder.start(100);
-
-        stopInterval =  setInterval(() => {
-            count++;
-            // use mp4box to create a new segment
-            
-            var blob = new Blob(recordedChunks, {type: "video/webm"});
-            fetch('http://localhost:5000/segment', {
-                method: 'POST',
-                body: blob
-            }).then(res => res.json())
-            .then(response => console.log('Success:', JSON.stringify(response)))
-            .catch(error => console.error('Error:', error));
-
-            
-            var url =  URL.createObjectURL(blob);
-            var a = document.createElement("a");
-            document.body.appendChild(a);
-            a.style = "display: none";
-            a.href = url;
-            a.download = `segment${count}.webm`;
-            a.click();
-            setTimeout(function() { URL.revokeObjectURL(url); }, 100); 
-            recordedChunks = [];
-          }, 3000);
-  }
-
-  function stop(){
-    clearInterval(stopInterval);
-    theRecorder.stop();
-    theStream.getTracks().forEach(track => { track.stop(); });
-  }
-
-  function download() {
-    theRecorder.stop();
-    theStream.getTracks().forEach(track => { track.stop(); });
-  
-    var blob = new Blob(recordedChunks, {type: "video/webm"});
-    var url =  URL.createObjectURL(blob);
+    var url = URL.createObjectURL(blob);
     var a = document.createElement("a");
     document.body.appendChild(a);
     a.style = "display: none";
     a.href = url;
-    a.download = 'test.webm';
+    a.download = `segment${count}.webm`;
     a.click();
-    // setTimeout() here is needed for Firefox.
-    setTimeout(function() { URL.revokeObjectURL(url); }, 100); 
-  }
+    setTimeout(function () {
+      URL.revokeObjectURL(url);
+    }, 100);
+    recordedChunks = [];
+  }, 3000);
+}
 
+function stop() {
+  clearInterval(stopInterval);
+  theRecorder.stop();
+  theStream.getTracks().forEach((track) => {
+    track.stop();
+  });
+}
+
+function reset() {
+  fetch("http://localhost:5000/");
+}
+
+function view() {
+  const mediaSource = new MediaSource();
+  const video = document.getElementById("myVideo");
+  video.src = URL.createObjectURL(mediaSource);
+
+  mediaSource.addEventListener("sourceopen", () => {
+    const sourceBuffer = mediaSource.addSourceBuffer(
+      'video/webm; codecs="vp8, opus"'
+    );
+
+    for (let i = 1; i < 3; i++) {
+      fetch(`http://localhost:5000/segment/${i}`, {
+        method: "GET",
+        headers: {
+          "Content-Type":
+            "multipart/form-data; boundary=----WebKitFormBoundaryFHADkzAysWPfmhB3",
+        },
+      })
+        .then((response) => response.arrayBuffer())
+        .then((segment) => {
+          console.log(segment);
+        });
+    }
+  });
+}
+
+function download() {
+  theRecorder.stop();
+  theStream.getTracks().forEach((track) => {
+    track.stop();
+  });
+
+  var blob = new Blob(recordedChunks, { type: "video/webm" });
+  var url = URL.createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = "test.webm";
+  a.click();
+  // setTimeout() here is needed for Firefox.
+  setTimeout(function () {
+    URL.revokeObjectURL(url);
+  }, 100);
+}
--- server/app.js
+++ server/app.js
@@ -1,8 +1,78 @@
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql");
+const multer = require("multer");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "videostreaming",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  console.log("MySql Connected...");
+});
+
+var cors = require("cors");
 const app = express();
 
-app.get('/', (req, res) => res.end('login'));
-app.post('/segment', (req, res) => res.end('register'));
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  let sql = "DELETE FROM segment";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Database cleared...");
+  });
+});
+
+app.get("/createdb", (req, res) => {
+  let sql = "CREATE DATABASE videostreaming";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Database created...");
+  });
+});
+
+app.get("/createsegmenttable", (req, res) => {
+  let sql =
+    "CREATE TABLE segment(id int AUTO_INCREMENT, segment BLOB, segment_id int, PRIMARY KEY(id))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("segment table created...");
+  });
+});
+
+app.get("/segment/:id", (req, res) => {
+    let sql = `SELECT * FROM segment WHERE segment_id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
+
+app.post("/segment", upload.single("video"), (req, res) => {
+  const segment_id = +req.headers["x-segment-id"];
+  let segment = { segment: req.file, segment_id: segment_id };
+  let sql = "INSERT INTO segment SET ?";
+  db.query(sql, segment, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(`Segment ${segment_id} uploaded...`);
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on  ${PORT}`));
